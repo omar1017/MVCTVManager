6b0f4d0d1c09b5dafcbd5de598722c043aedad27
omar1017 <omarnhnh@outlook.sa>
2024-08-19 23:07:54 +0300

    create mvc project

---
+++ MVCTVManagerProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCTVManagerProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
